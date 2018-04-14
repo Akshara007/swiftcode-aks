132971b21c7aaccf8905ca9414228fd0f8dbf6d2
Akshara007 <aksharagdutt@gmail.com>
2018-04-14 12:14:08 +0000

    Message Display

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,4 +2,25 @@ var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
 
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hello'
+		},
+        {
+            'sender': 'BOT',
+            'text': 'Hi What can I do for u ???'
+		},
+        {
+            'sender': 'USER',
+            'text': 'Give me a serach result'
+		},
+        {
+            'sender': 'BOT',
+            'text': 'Ofcourse !!!'
+
+			}
+
+	];
+
 });
