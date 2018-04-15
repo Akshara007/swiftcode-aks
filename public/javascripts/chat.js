466a38e362ab15df27b53476b0377cf5945053df
Akshara007 <aksharagdutt@gmail.com>
2018-04-15 07:48:27 +0000

    UI

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,11 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
+app.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('purple')
+        .accentPalette('orange');
+});
+
 app.controller('chatController', function ($scope) {
 
     $scope.messages = [
